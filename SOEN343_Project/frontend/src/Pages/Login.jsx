c8e9b12808585971524eb15669c774fac3678865
Tahsin Islam <tahsin_i12@outlook.com>
2024-11-07 19:11:38 -0500

    Finished login UI

--- SOEN343_Project/frontend/src/Pages/Login.jsx
+++ SOEN343_Project/frontend/src/Pages/Login.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './CSS/Login.css'
+import facebook_logo from '../Components/Assets/Facebook.png'
+import google_logo from '../Components/Assets/Google.png'
+import twitter_logo from '../Components/Assets/TwitterX.png'
+import apple_logo from '../Components/Assets/Apple Logo.png'
+import amazon_logo from '../Components/Assets/Amazon.png'
+
 export const Login = () => {
   return (
-    <div className="loginForm">
-        <form className="login">
+    <div className="loginPage">
+        <form className="loginForm">
             <div className="loginFormElement">
                 <h2>Log In</h2>
                 <label>Email or Username</label>
@@ -12,8 +18,15 @@ export const Login = () => {
                 <label>Password</label>
                 <input type="password"></input>
                 <br/>
-                <button type="submit" className="loginButton">Login</button>
-                <p><span>Or with</span></p>
+            </div>
+            <button type="submit" className="loginButton">Login</button>
+            <p><span>Or with</span></p>
+            <div className="company-logos">
+              <img src={google_logo} alt="" className="google" />
+              <img src={facebook_logo} alt="" className="facebook" />
+              <img src={twitter_logo} alt="" className="twitter" />
+              <img src={apple_logo} alt="" className="apple" />
+              <img src={amazon_logo} alt="" className="amazon" />
             </div>
         </form>
     </div>
