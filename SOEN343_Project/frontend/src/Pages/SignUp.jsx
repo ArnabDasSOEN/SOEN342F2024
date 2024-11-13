1c2e4d2d658551fe8d851563fb83ba055cd65e8a
Tahsin Islam <tahsin_i12@outlook.com>
2024-11-13 14:32:26 -0500

    formatting SignUp and Login

--- SOEN343_Project/frontend/src/Pages/SignUp.jsx
+++ SOEN343_Project/frontend/src/Pages/SignUp.jsx
@@ -7,17 +7,23 @@ export const SignUp = () => {
         <div className="signup">
             <h2>Sign Up</h2>
             <form className="signup-page">
-                <label className="name">Your Name</label>
+                <label htmlFor="name">Name</label>
                 <input type="text" />
                 <br/>
-                <label className='email'>Email</label>
+                <label htmlFor="phone_number">Phone Number</label>
+                <input type="tel" />
+                <br/>
+                <label className='email'>Email Address</label>
                 <input type="email" />
                 <br/>
-                <label className='password'>Password</label>
+                <label htmlFor='password'>Password</label>
+                <input type="password" />
+                <br/>
+                <label htmlFor="confirm_password">Confirm Password</label>
                 <input type="password" />
             </form>
             <button>Continue</button>
-            <p className="loginsignup-login">Already have an account? <Link>Login here</Link></p>
+            <p className="loginsignup-login">Already have an account? <Link to='/login' className='login-link'>Login here</Link></p>
             <div className="signup-agree">
                 <input type="checkbox" />
                 <p>By continuing, I agree to the terms of use & privacy policy</p>
